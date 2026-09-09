007affe188e6322122e6e0faa4b63e974a206c3c
Axel Solano Castillo <168589851+Theisoluck@users.noreply.github.com>
2026-09-08 18:47:16 -0600

    Agrego el formulario de negociacion

--- js/script.js
+++ js/script.js
@@ -13,3 +13,60 @@ function desatarCaos() {
 
 const botonCaos = document.getElementById('chaosButton');
 botonCaos.addEventListener('click', desatarCaos);
+
+
+const formulario = document.getElementById("formularioInnecesario");
+
+const nivel = document.getElementById("nivel");
+const nivelTexto = document.getElementById("nivelTexto");
+
+const resultado = document.getElementById("resultado");
+
+nivel.addEventListener("input", function () {
+    nivelTexto.textContent = nivel.value + "%";
+});
+
+formulario.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const nombre = document.getElementById("nombre").value;
+    const animal = document.getElementById("animal").value;
+    const color = document.getElementById("color").value;
+    const nivelPatos = document.getElementById("nivel").value;
+    const excusa = document.getElementById("excusa").value;
+
+    document.body.style.backgroundColor = color;
+
+    resultado.style.display = "block";
+
+    resultado.innerHTML = `
+        <h2>Resultados completamente inútiles</h2>
+
+        <p>
+            <strong>Superhéroe:</strong>
+            ${nombre}
+        </p>
+
+        <p>
+            <strong>Futuro gobernante del mundo:</strong>
+            ${animal}
+        </p>
+
+        <p>
+            <strong>Preparación contra patos:</strong>
+            ${nivelPatos}%
+        </p>
+
+        <p>
+            <strong>Excusa:</strong>
+            ${excusa || "No proporcionaste ninguna excusa."}
+        </p>
+
+        <p>
+            Después de un análisis extremadamente científico,
+            hemos determinado que esta información no sirve para nada.
+        </p>
+    `;
+});
+
